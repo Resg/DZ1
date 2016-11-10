70ea9ad3f5faa363c4b97d4dee5935867bfdce1a
Resggg <sergey@panichev.su>
2016-11-10 21:04:40 +0300

    фыв

--- Server_boost/Server_boost/Server_boost.cpp
+++ Server_boost/Server_boost/Server_boost.cpp
@@ -2,32 +2,29 @@
 //
 
 #include "stdafx.h"
-#include <boost\asio.hpp>
-#include <iostream>
-//#include <memory>
-#include <boost\thread.hpp>
-//#include <vector>
+#include "Header.h"
 
-using namespace boost::asio;
-//typedef boost::shared_ptr<ip::tcp::socket> socket_ptr;
-//std::vector <ip::tcp::socket> connections;
-//std::vector <ip::tcp::socket>::iterator iter;
-ip::tcp::socket *connections;
-ip::tcp::acceptor *acc;
+
+
+//typedef boost::shared_ptr<_client> client_ptr;
+std::vector <boost::shared_ptr<_client>> connections;
+std::vector <boost::shared_ptr<_client>>::iterator iter;
+
+//ip::tcp::acceptor *acc;
 int con_count;
-void client_session(int _ID)
+void client_session(boost::shared_ptr<_client> client)
 {
 	while (true)
 	{
 		char data[4096];
 		memset(data, 0, 4096);
-		size_t len = connections[_ID].read_some(buffer(data));
+		size_t len = client->sock().read_some(buffer(data));
 		if (len > 0)
-			for (int itID=0;itID<con_count;itID++)
+			for (iter = connections.begin(); iter < connections.end(); iter++)
 			{
-				write(connections[itID], buffer(data, strlen(data)));
-
+				write((*iter)->sock(), buffer(data, strlen(data)));
 			}
+		Sleep(100);
 	}
 }
 
@@ -37,20 +34,23 @@ int main()
 	try
 	{
 		con_count = 0;
-		io_service service;
 		ip::tcp::endpoint ep(ip::tcp::v4(), 7770);
 		ip::tcp::acceptor acc(service, ep);
-		boost::asio::ip::tcp::acceptor acceptor(io_service);
-		acc.open(ep.protocol());
-		acc.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
-		acc.bind(ep);
-		acc.listen();
+		size_t len;
+		//connect.bind;
+		//boost::asio::ip::tcp::acceptor acceptor(io_service);
 		for (;; con_count++)
 		{
 			std::cout << con_count;
-			connections = new ip::tcp::socket(service);
-			//acc.accept(connections[con_count]);
-			boost::thread(boost::bind(client_session, con_count));
+			boost::shared_ptr<_client> client(new _client);
+			acc.accept(client->sock());
+			char data[4096];
+			memset(data, 0, 4096);
+			len= client->sock().read_some(buffer(data));
+			if (len > 0)
+				std::string _d = data;
+			boost::thread(boost::bind(client_session, &client));
+			connections.push_back(client);
 			Sleep(100);
 		}
 		
